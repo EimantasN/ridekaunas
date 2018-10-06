8d4b9f5ce86470763d6d75c3453bfc7bd73df98c
lukjok <lukjok7@gmail.com>
2018-10-07 00:32:51 +0300

    Logo and NFC

--- app/src/main/java/com/example/pertrauktiestaskas/trulify/MainActivity.java
+++ app/src/main/java/com/example/pertrauktiestaskas/trulify/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.pertrauktiestaskas.trulify;
 
 import com.example.pertrauktiestaskas.methods.NfcClass;
+
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +20,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
@@ -21,6 +28,10 @@ import com.example.pertrauktiestaskas.methods.BusThread;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private String serialId = "";
+    final protected static char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    NfcAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,10 +61,74 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        adapter = NfcAdapter.getDefaultAdapter(this);
+        onNewIntent(this.getIntent());
+
 //        RootObject data = BusApiHandler.GetRouteData("54.901694", "23.961288", "54.893767", "23.925123");
 //        Log.d("TG", data.toString());
-        Intent launchNewIntent = new Intent(MainActivity.this, NfcClass.class);
-        startActivityForResult(launchNewIntent, 0);
+        //Intent launchNewIntent = new Intent(MainActivity.this, NfcClass.class);
+        //startActivityForResult(launchNewIntent, 0);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        PendingIntent pendingIntent     = PendingIntent.getActivity(this,0,new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
+        IntentFilter[] intentFilters    = { new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED) };
+
+        adapter.enableForegroundDispatch(   this,
+                pendingIntent,
+                intentFilters,
+                new String[][]{
+                        new String[]{"android.nfc.tech.NfcA"}
+                });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (adapter != null)
+        {
+            try {
+                adapter.disableForegroundDispatch(this);
+            }
+            catch (NullPointerException e) {
+            }
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        Toast.makeText(this,""+intent.getAction(), Toast.LENGTH_LONG).show();
+        super.onNewIntent(intent);
+        String action = intent.getAction();
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equalsIgnoreCase(action)) {
+            try {
+                byte[] tagId = tag.getId();
+                serialId = bytesToHex(tagId);
+
+            } catch (NullPointerException ex) {
+                ex.printStackTrace();
+                serialId = "ERROR";
+            }
+        } else {
+            Toast.makeText(this, "This tag is not supported. Action: " + action, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 
     @Override
