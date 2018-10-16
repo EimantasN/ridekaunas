e033a81cbdf4e284125ca0308f1844a646f40cd8
lukjok <lukjok7@gmail.com>
2018-10-16 11:03:59 +0300

    Add Card Activity

---
+++ app/src/main/java/com/example/pertrauktiestaskas/trulify/AddCardInfo.java
@@ -0,0 +1,13 @@
+package com.example.pertrauktiestaskas.trulify;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddCardInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card_info);
+    }
+}
